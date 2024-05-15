04f40da891c154f0f0a5253bed75ab8f733e0604
Annanas1k <radu.pavlovschi@gmail.com>
2024-05-15 14:06:29 +0300

    add controllers: Prices, OurProjects, ContactUs and all twig compnents

---
+++ src/Controller/OurProjectsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class OurProjectsController extends AbstractController
+{
+    #[Route('/our/projects', name: 'app_our_projects')]
+    public function index(): Response
+    {
+        return $this->render('our_projects/index.html.twig', [
+            'controller_name' => 'OurProjectsController',
+        ]);
+    }
+}
