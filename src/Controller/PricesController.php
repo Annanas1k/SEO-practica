a5e81dc575e329c1c6d68ea7d10ed54f5e1414e7
Annanas1k <radu.pavlovschi@gmail.com>
2024-05-25 16:30:30 +0300

    add price entity(model) and create tabel price with migration

--- src/Controller/PricesController.php
+++ src/Controller/PricesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PriceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,22 @@ use Symfony\Component\Routing\Attribute\Route;
 class PricesController extends AbstractController
 {
     #[Route('/prices', name: 'app_prices')]
-    public function index(): Response
+    public function index(PriceRepository $priceRepository): Response
     {
+        $prices = $priceRepository->findAll();
+        $categories = [];
+
+        // Colectăm toate categoriile unice
+        foreach ($prices as $price) {
+            $categories[] = $price->getCategory();
+        }
+
+        // Eliminăm duplicatelor
+        $categories = array_unique($categories);
+
         return $this->render('prices/index.html.twig', [
-            'controller_name' => 'PricesController',
+            'prices' => $prices,
+            'categories' => $categories,
         ]);
     }
 }
