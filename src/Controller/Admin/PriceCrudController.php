ad7e39d58fc7cfbd94cb9f31e44928e04890e35e
Annanas1k <radu.pavlovschi@gmail.com>
2024-06-08 13:27:04 +0300

    add details about servicies in array object

--- src/Controller/Admin/PriceCrudController.php
+++ src/Controller/Admin/PriceCrudController.php
@@ -20,7 +20,6 @@ class PriceCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('name'),
             TextEditorField::new('description'),
             TextField::new('category'),
